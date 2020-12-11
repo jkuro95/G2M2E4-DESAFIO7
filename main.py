1a367a424ad04fb78d5c2e130069269eab1e77b9
jkuro95 <jkuro85@gmail.com>
2020-12-11 03:14:26 -0500

    Se agrega la funcionalidad de aumentar cantidad a los productos

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from db.user_db import get_user, get_all_users
-from db.products_db import ProductInDB, get_all_products, get_single_product, create_product
+from db.products_db import ProductInDB, get_all_products, get_single_product, create_product, change_product
 
 from models.user_models import UserIn, UserOut
-from models.product_models import ProductOut
+from models.product_models import ProductOut, ProductAddCantIn, ProductAddCantOut
 
 # import datetime
 from fastapi import FastAPI, HTTPException
@@ -63,3 +63,25 @@ async def read_product(product_id: int):
 async def add_product(product: ProductInDB):
     product_added_to_db = create_product(product)
     return product_added_to_db
+
+# aumentar la cantidad del producto
+@api.put("/productos/{product_id}")
+async def add_cant(product_add_cant_in: ProductAddCantIn):
+
+    product_in_db = get_single_product(product_add_cant_in.product_id)
+
+    if product_in_db == None:
+        raise HTTPException(status_code=404, detail="El producto no existe")
+
+    product_in_db.cantidad = product_in_db.cantidad + product_add_cant_in.cantidad
+
+    product_change_to_db = change_product(product_in_db, product_add_cant_in.product_id)
+
+    product_add_cant_out = ProductAddCantOut(**{"nombre": product_change_to_db.nombre,
+                                                "cantidad": product_change_to_db.cantidad})
+
+    return product_add_cant_out
+
+
+
+
